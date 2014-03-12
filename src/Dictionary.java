2564bf61f1f48a897645fa9789c9bed4d47b49cb
kazimad <kazimad.kr@gmail.com>
2014-03-12 21:29:56 +0200

    Initial commit

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
+
 
 
 /**
@@ -14,15 +15,15 @@ public class Dictionary  {
     public static void main (String []args) throws IOException {
 
         String num;
-        int count1,count2,count3,count4,count5,count6,count7,count8,count9,count0;
-        TreeMap <Integer,Integer>SetOfInt = new TreeMap<Integer, Integer>();
+
+        HashSet<Integer> SetOfInt = new HashSet<Integer>();
         BufferedReader br = new BufferedReader(new FileReader("pass.txt"));
         while ((num = br.readLine()) !=null){
             StringTokenizer stnum = new StringTokenizer(num);
             while (stnum.hasMoreTokens()){
                 String token = stnum.nextToken();
                 Integer  q = Integer.parseInt(token);
-                SetOfInt.put( q ,q);
+                SetOfInt.add(q);
             }
 
         }
